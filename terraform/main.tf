a4c27ceaf1655aefe5b583c3b3f6b1b327edceac
pailaghising <papilaghising1111@gmail.com>
2024-05-30 13:59:33 +0545

    my first commit to provision ec2 using terrafom

---
+++ terraform/main.tf
@@ -0,0 +1,19 @@
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  name = "amazon-linux-instance"
+  instance_type               = var.instance_type
+  key_name                    = "papila-intern"
+  monitoring                  = true
+  ami                         = var.ami_id
+  vpc_security_group_ids      = var.sg_id
+  subnet_id                   = var.subnet_id
+  create_iam_instance_profile = var.create_iam_instance_profile
+  iam_role_description = "SSM Role for accessing EC2 instance"
+
+#   tags = {
+#     terraform   = "true"
+#     owner       = "papila.ghising"
+#     silo        = "intern"
+#     project     = "amazon_linux"
+#   }
+}
