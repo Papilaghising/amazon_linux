a99208098f770984fda1f665c2961c9ea12f9ddb
pailaghising <papilaghising1111@gmail.com>
2024-05-31 14:07:52 +0545

    changed ssm policy

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,7 +1,6 @@
-
 #!/bin/bash
-sudo yum update -y
-sudo yum install -y ansible
+
+sudo yum install  ansible
 # Install  unzip
 sudo yum install  unzip  -y
 
@@ -11,7 +10,5 @@ aws s3 cp s3://ghising-s3-bucket/ansible.zip /var/ansible.zip
 # Unzip the file
 unzip /var/ansible.zip -d /var/ansible
 
-
-sleep 30
-sudo ansible-playbook -i /var/ansible/ansible/ansible/aws_ec2.yml /var/ansible/ansible/ansible/playbook.yml
+sudo ansible-playbook -i /var/ansible/ansible/ansible/aws_ec /var/ansible/ansible/ansible/playbook.yml
               
