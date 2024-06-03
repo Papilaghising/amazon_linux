927f9822d57b056a7cf20d76dda465479e47f9b2
pailaghising <papilaghising1111@gmail.com>
2024-06-03 11:24:15 +0545

    website hosting chech commit

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -7,7 +7,7 @@ sudo yum install  unzip  -y
 # Download the ZIP file from S3
 aws s3 cp s3://ghising-s3-bucket/ansible.zip /var/ansible.zip
 
-# Unzip the file
+
 unzip /var/ansible.zip -d /var/ansible
 
 sudo ansible-playbook /var/ansible/ansible/ansible/playbook.yml
