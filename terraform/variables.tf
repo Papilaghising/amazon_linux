648f96689f5ce33fcc06cac4970a0c87c3d387ad
pailaghising <papilaghising1111@gmail.com>
2024-06-03 09:32:10 +0545

    website hosting chech commit

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,6 +2,6 @@ variable "ami_id" {}
 variable "instance_type" {}
 variable "sg_id" {}
 variable "subnet_id" {}
-#variable "create_iam_instance_profile" {
+#variale "create_iam_instance_profile" {
 variable "ssm_policy" {}
 variable "region"{}
