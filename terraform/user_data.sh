846344d7f6abbdb3adfdab527c2bca337468cd4a
pailaghising <papilaghising1111@gmail.com>
2024-06-03 09:44:27 +0545

    website hosting chech commit

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -11,4 +11,5 @@ aws s3 cp s3://ghising-s3-bucket/ansible.zip /var/ansible.zip
 unzip /var/ansible.zip -d /var/ansible
 
 sudo ansible-playbook /var/ansible/ansible/ansible/playbook.yml
+
               
