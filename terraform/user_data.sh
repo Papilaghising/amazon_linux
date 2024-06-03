d9ab55366acff87a7cd92cf3b530e0bba1a7a087
pailaghising <papilaghising1111@gmail.com>
2024-06-03 13:10:37 +0545

    installed nginx using yum'

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -4,7 +4,7 @@ sudo amazon-linux-extras install  ansible2 -y
 # Install  unzip
 sudo yum install  unzip  -y
 
-# Download the ZIP file from S3
+
 aws s3 cp s3://ghising-s3-bucket/ansible.zip /var/ansible.zip
 
 
